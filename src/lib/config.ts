b4830c75be20999e5a81317382d0037b0ab31fcf
Francisco López <fran.jlp26@gmail.com>
2022-08-15 13:29:42 -0600

    Add order endpoints to config file

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,3 +4,5 @@ export const SIGNUP_URL = BASE_URL + import.meta.env.VITE_API_SIGNUP
 export const USERLOGIN_URL = BASE_URL + import.meta.env.VITE_API_USERLOGIN
 export const INIT_URL = BASE_URL + import.meta.env.VITE_API_INIT
 export const REFRESH_TOKEN = BASE_URL + import.meta.env.VITE_API_REFRESH
+export const GET_ALL = BASE_URL + import.meta.env.VITE_API_GETALL
+export const UPDATE_ORDER = BASE_URL + import.meta.env.VITE_API_UPDATEORDER
