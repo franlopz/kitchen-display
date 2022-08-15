4785d5752546727c6866b98065d18459ffed11fc
Francisco López <fran.jlp26@gmail.com>
2022-08-15 13:26:49 -0600

    Add props to Header and style scroll bar

--- src/components/main/OrderCard/Header.tsx
+++ src/components/main/OrderCard/Header.tsx
@@ -5,24 +5,33 @@ import styles from './OrderCard.module.css'
 interface Props {
   number: number
   type: string
-  waiter: string
+  seller: string
   table: string
-  date: string
+  createdAt: string
   screen: string
+  isVoided: boolean
 }
 
-const Header: FC<Props> = ({ number, type, waiter, table, date, screen }) => {
-  const { getStatus, elapsedTime } = useOrders({ date })
-
+const Header: FC<Props> = ({
+  number,
+  type,
+  seller,
+  table,
+  createdAt,
+  screen,
+  isVoided,
+}) => {
+  const { getStatus, elapsedTime } = useOrders({ createdAt })
   return (
     <div className={`${styles.header} ${getStatus(screen)}`}>
       <div className={styles['header-row']}>
         <p className={styles.number}>#{number}</p>
         <p className={styles.type}>{type}</p>
-        <p>{waiter}</p>
+        <p>{seller}</p>
       </div>
       <div className={styles['header-row']}>
         <p>{table}</p>
+        <h2>{isVoided ? 'ANULADO' : ''}</h2>
         <p>{elapsedTime}</p>
       </div>
     </div>
